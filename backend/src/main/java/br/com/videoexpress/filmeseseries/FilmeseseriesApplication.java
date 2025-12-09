4ab1ebca79b3873e0bf12610d7b4cd318aed6146
BrunoMoreira <bruno.moreira@lecom.com.br>
2025-12-08 22:59:31 -0300

    project created

---
+++ backend/src/main/java/br/com/videoexpress/filmeseseries/FilmeseseriesApplication.java
@@ -0,0 +1,13 @@
+package br.com.videoexpress.filmeseseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeseseriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmeseseriesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/videoexpress/filmeseseries/FilmeseseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.videoexpress.filmeseseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmeseseriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
