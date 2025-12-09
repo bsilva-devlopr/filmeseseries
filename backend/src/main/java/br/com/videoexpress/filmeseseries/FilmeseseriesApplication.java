b099008d5d7acadf11ecde710d6409d37d6fddf5
BrunoMoreira <bruno.moreira@lecom.com.br>
2025-12-08 23:12:54 -0300

    Atualização

--- backend/src/main/java/br/com/videoexpress/filmeseseries/FilmeseseriesApplication.java
+++ backend/src/main/java/br/com/videoexpress/filmeseseries/FilmeseseriesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmeseseriesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FilmeseseriesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FilmeseseriesApplication.class, args);
+    }
 
 }
